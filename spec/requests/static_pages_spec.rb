5dfe1b889a2a24b7544f7986cef1311d6bd70b5c
Soren Berg <soren@vetrounds.com>
2014-03-15 11:58:58 -0700

    fix spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home Page" do
-    it "should have the content 'So how exactly does it work ?'" do
+    it "should have the content 'How does it work ?'" do
       visit root_url
-      expect(page).to have_content("So how does it work ?")
+      expect(page).to have_content("How does it work ?")
     end
 
     it "should have the page title 'Home | Vetrounds'" do
