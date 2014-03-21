5e5d785d1d29bda825a2db6e67abdeab1879036c
Soren Berg <soren@vetrounds.com>
2014-03-21 08:47:10 -0700

    add homepage test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,14 +2,24 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home Page" do
-    it "should have the content 'How does it work ?'" do
+    before do
       visit root_url
+    end
+
+    it "should have the content 'How does it work ?'" do
       expect(page).to have_content("How does it work ?")
     end
 
     it "should have the page title 'Home | Vetrounds'" do
-      visit root_url
       expect(page).to have_title("Home | Vetrounds")
+    end
+
+    it "should allow asking question" do
+      test = "test question"
+      fill_in "question", with: test, match: :first
+      click_button "Ask Question", match: :first
+      current_path.should eq(home_question_post_path)
+      page.should have_content test
     end
   end
 end
