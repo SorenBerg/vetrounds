4d2f518065bb3140c11a5ee021345618d571df20
Soren Berg <soren@vetrounds.com>
2014-04-26 00:08:17 -0700

    carousel improvements

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     track_event("Load Homepage")
     @active_content = Answer.take(5)
+    @top5 = User.where("is_vet = 't' AND is_admin != 't'").take(5) * 5
   end
 
   def home2
