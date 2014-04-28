aef0fb9cca3710ccb80dfb30245c4044a24b994b
Soren Berg <soren@vetrounds.com>
2014-04-27 21:50:43 -0700

    fix front page metrics

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,20 @@
 class StaticPagesController < ApplicationController
   def home
     track_event("Load Homepage")
-    @active_content =  Answer.take(5)
-    @top5 = User.where("is_vet = 't' AND is_admin != 't'").take(5) * 5
+    answers = Answer.find(:all)
+    answers.find_all{|a| a.question.is_consult == false }.sort_by{|a| a.agreements.count}.take(5)
+    @active_content =  answers
+    
+    vets = User.where("is_vet = 't' AND is_admin != 't'")
+    @top5 = vets.reverse.sort_by { |vet| vet.answers.count }.take(5)
+
+    # @top5 = [
+    # 		User.find(2),
+    # 		User.find(62),
+    # 		User.find(76),
+    # 		User.find(110),
+    # 		User.find(109)
+    # 	]
   end
 
   def terms
