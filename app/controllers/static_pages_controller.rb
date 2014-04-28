cdb159fcf5d6b942ea4c6041108135723023a9e1
Soren Berg <soren@vetrounds.com>
2014-04-28 11:58:22 -0700

    fix active_content and top5

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,19 +2,16 @@ class StaticPagesController < ApplicationController
   def home
     track_event("Load Homepage")
     answers = Answer.find(:all)
-    answers.find_all{|a| a.question.is_consult == false }.sort_by{|a| a.agreements.count}.take(5)
-    @active_content =  answers
-    
-    vets = User.where("is_vet = 't' AND is_admin != 't'")
-    @top5 = vets.reverse.sort_by { |vet| vet.answers.count }.take(5)
+    @active_content = answers.select{|a| a.question.is_consult == false and a.agreements.count > 0 and a.user.id != 35 }.sample(3)
+    if @active_content.length < 3
+    	@active_content = @active_content * 3
+    end
 
-    # @top5 = [
-    # 		User.find(2),
-    # 		User.find(62),
-    # 		User.find(76),
-    # 		User.find(110),
-    # 		User.find(109)
-    # 	]
+    vets = User.where("is_vet = 't' AND is_admin != 't'").order(:id)
+    @top5 = vets.select { |vet| vet.id != 35 and vet.id != 72 }.take(5)
+  	if @top5.length < 5
+    	@top5 = @top5 * 5
+    end
   end
 
   def terms
